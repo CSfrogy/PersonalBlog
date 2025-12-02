8509039e16591d3e82d784d20651cc0ebcd54f30
CSfrogy <lonelyguse@gmail.com>
2025-12-02 15:28:32 +0800

    Correct redirect after auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,12 +35,8 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
-                    <h2 class="text-2xl font-semibold mb-6">Create a New Post</h2>
-                    <!-- Your post creation form will go here -->
-                    <form action="{{ route('posts.store') }}" method="POST">
-                        @csrf
-                        <!-- Add your form fields here -->
-                    </form>
+                    <h2 class="text-2xl font-semibold mb-6">For You</h2>
+
                 </div>
             </div>
         </div>
